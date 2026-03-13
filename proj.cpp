fbcc4c81bde9476ec8f729588c122d7b84a29e1a
gigastoned <2500973@sit.singaporetech.edu.sg>
2026-03-14 00:47:39 +0800

    Update proj.cpp

--- proj.cpp
+++ proj.cpp
@@ -1,25 +1,7 @@
 #include "Users.h"
-#include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-#include <fstream>
-
 using namespace std;
 
-void menu();
-Users user_create();
-bool password_verifier(string user_password);
-
 int main() {
-    map<string, Users> user_database;
-    vector<string> all_NRIC;
-    User_data dataObj(user_database, all_NRIC);
-    Users userObj;
-    dataObj.dummy_data();
-    dataObj.retrieve_user();
-    dataObj.create_user();
-    dataObj.retrieve_user();
-    dataObj.output_database();
+    program();
     return 0;
 }
