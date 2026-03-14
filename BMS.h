368a7934eb2b091ec0c17150cdde02647580353b
kuudan <anthonytankaheng@gmail.com>
2026-03-14 17:43:28 +0800

    Integrated Users and Librarian. Can proceed to integration. Please test for bugs or oversight.
    - renamed BMS --> BMS.L for librarian/database related
    - created BMS_U for user-related commands
    - removed C++_BookManagementUsers.cpp, integrated into BMS.h
    - edited main.cpp to switch between BMS User or BMS Librarian
    - please remind me to somehow fix launch.json and tasks.json when integrated everything else (need to add the relevant file paths, and create a build folder)
    - edited LibraryBooks.txt as there was duplicate serial number. Please remind anyone in the group to like, add new data into this text for the purpose of fulfilling the requirements
    - inform me if you want to work with ios::bin.
    - will add comments later.

--- BMS.h
+++ BMS.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef BOOKS
+#define BOOKS
 #include <iostream> //basic
 #include <string> //string-related func
 #include <iomanip> //manipulators
@@ -20,11 +21,37 @@ class Books{
         int BMS_L(const char& v = 0, int cat=0, bool sort=0);
         int BMS_L(int RTS=0, const char& v=0);
         int BMS();
+        std::string getTitle() const { return title; }
         std::string getSerial() const { return serial; }
+        std::string getGenre() const { return genre; }
+        std::string getVenue() const { return venue; }
         struct BookData{
             std::vector<Books> book; //declared vector for book class -- allows program to dynamically allocate memory to the class during runtime
             int size[3] = {0,0,0}; //3 sizes to be used for iomanip -- text output manipulation
         };
+        Books::BookData loadBooks(const char* Filename="LibraryBooks.txt", const char* venueFilter=nullptr);
+        void printHeader(BookData data);
+};
+#endif
+
+#ifndef BMS_U
+#define BMS_U
+class BookManagementUser {
+private:
+    std::vector<std::string> scannedBooks; // Stores serials for the current session
+    Books::BookData data;
+
+    // Action: "Reduce book count in database"
+    void updateDatabase(std::string serial, Books::BookData data);
+
+    // Action: "Error message"
+    void showErrorMessage(std::string message);
+
+public:
+    static constexpr int MAX_BOOKS = 2;               // Flowchart logic: "Books taken >= 2?"
+    void startSession();
+    void printReceipt();
+    std::vector<std::string> const getSerial(){ return scannedBooks; }
 };
 
-
+#endif
--- main.cpp
+++ main.cpp
@@ -1,8 +1,46 @@
 #include "BMS.h"
-#include "BMS.cpp"
+//#include "BMS_L.cpp"
+//#include "BMS_U.cpp"
+/*
+when actually compiling
+*/
 
-Books bookobj;
+Books lib;
+BookManagementUser session;
+using namespace std;
 
 int main(){
-    bookobj.BMS();
+    char stuff='0';
+    
+    while(stuff!='N'){
+        cout << "are you user? Y/N: ";
+        cin >> stuff;
+        stuff = toupper(stuff);
+        if (stuff == 'Y'){
+            session.startSession();
+        }
+        else if (stuff == 'N') {
+            lib.BMS();
+        }
+        else {
+            cout << "invalid input. try again: ";
+        }
+        cin.clear();
+        cin.ignore();
+        cout << "continue? Y/N: ";
+        cin >> stuff;
+        stuff = toupper(stuff);
+        if (stuff == 'Y'){
+            cout << "continuing\n";
+        }
+        else if (stuff == 'N') {
+            cout << "ending system\n";
+        }
+        else {
+            cout << "invalid input. try again: ";
+        }
+        cin.clear();
+        cin.ignore();
+    }
+    return 0;
 }
