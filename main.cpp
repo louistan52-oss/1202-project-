5d85693b446d173fc33b0f64b09108a9c0ad178f
Chuan Chew <167091212+ChuanChew@users.noreply.github.com>
2026-03-16 14:18:17 +0800

    Update main.cpp
    Updated from Venue (Anthony) for testing purposes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include "BMS.h"
 #include "Users.h"
+#include "Robot_Transport_System.h"
 
 using namespace std;
 
+SystemController sc;
+
 int main(){
+    srand(static_cast<unsigned int>(time(0)));
+    //cout << "Initializing Robot Transport System... Please type Q when done:\n";
+    //sc.run();
     USERS::program();
     return 0;
 }
